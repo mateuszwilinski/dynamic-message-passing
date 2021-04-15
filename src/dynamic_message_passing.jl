c2e07ef727efcc8bd2c9f4464b4bb06b21f00194
mateuszwilinski <mateuszwilinski@o2.pl>
2021-04-15 16:39:41 -0600

    included the case when (1 - alpha * message) == 0.0, which could crash the smarter/faster implementation of DMP

--- src/dynamic_message_passing.jl
+++ src/dynamic_message_passing.jl
@@ -2,12 +2,12 @@
 """
     dynamic_messsage_passing(g, p0, T)
 
-Compute the marginals and messages for cascade of length T on a graph g with initial condition p0
+Computes the marginals and messages for cascade of length T on a graph g with initial condition p0
 """
 function dynamic_messsage_passing(g::Graph, p0::Array{Float64, 1}, T::Int64)
     # initialising marginals and messages
     marginals = zeros(Float64, T, g.n)
-    messages = Dict{Array{Int64,1}, Array{Float64,1}}()
+    messages = Dict{Array{Int64, 1}, Array{Float64, 1}}()
     for edge in keys(g.edgelist)
         messages[edge] = zeros(Float64, T)
         messages[reverse(edge)] = zeros(Float64, T)
@@ -22,21 +22,40 @@ function dynamic_messsage_passing(g::Graph, p0::Array{Float64, 1}, T::Int64)
     # DMP
     for t in 2:T
         for i in 1:g.n
-            marginals[t, i] = (1.0 - p0[i])
+            marginals[t, i] = 1.0 - p0[i]
             for neighbor in g.neighbors[i]
                 marginals[t, i] *= (1.0 - g.edgelist[sort(Int64[neighbor, i])] *
                                     messages[Int64[neighbor, i]][t-1])
             end  # TODO: Przyjrzyj sie ponizszemu, czy nie da sie tego poprawic
-            marginals[t, i] = max(marginals[t-1, i], 1.0 - marginals[t, i])
+            marginals[t, i] = max(marginals[t-1, i], 1.0 - marginals[t, i])  # numerical safeguard
         end
-        for (k, v) in messages  # TODO: uwzglednij mozliwosc, ze bedzie dzielenie przez zero (!!!)
-            v[t] = (1.0 - (1.0 - marginals[t, k[1]]) /
-                    (1.0 - g.edgelist[sort(Int64[k[1], k[2]])] *
-                     messages[Int64[k[2], k[1]]][t-1]))
-            v[t] = max(v[t], v[t-1])  # TODO: Przyjrzyj sie temu, czy nie da sie tego poprawic
+        for (k, v) in messages
+            temp_prob = 1.0 - g.edgelist[sort(Int64[k[1], k[2]])] * messages[Int64[k[2], k[1]]][t-1]
+            if temp_prob == 0.0
+                v[t] = get_message_hard_way(messages, g, p0, k, t)
+            else
+                v[t] = 1.0 - (1.0 - marginals[t, k[1]]) / temp_prob
+            end  # TODO: Przyjrzyj sie ponizszemu, czy nie da sie tego poprawic
+            v[t] = max(v[t], v[t-1])  # numerical safeguard
         end
     end
     return marginals, messages
+end
+
+"""
+    get_message_hard_way(messages, g, p0, k, t)
+
+Computes the message for edge 'k' at time 't', assuming initial condition p0
+"""
+function get_message_hard_way(messages::Dict{Array{Int64, 1}, Array{Float64, 1}}, g::Graph,
+                              p0::Array{Float64, 1}, k::Array{Int64, 1}, t::Int64)
+    message = 1.0 - p0[k[1]]
+    for neighbor in g.in_neighbors[k[1]]
+        if neighbor != k[2]
+            message *= 1.0 - g.edgelist[sort(Int64[neighbor, k[1]])] * messages[Int64[neighbor, k[1]]][t-1]
+        end
+    end
+    return 1.0 - message
 end
 
 """
