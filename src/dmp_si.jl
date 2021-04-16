d46653f786d47c17c25132ad0ae178e0639bf128
mateuszwilinski <mateuszwilinski@o2.pl>
2021-04-15 18:23:07 -0600

    updated the si model branch (test)

--- src/dmp_si.jl
+++ src/dmp_si.jl
@@ -1,6 +1,10 @@
-#=
-dmp_si:
-- Julia version: 
-- Author: mateuszwilinski
-- Date: 2021-04-15
-=#
+
+"""
+    dmp_si(g, p0, T)
+
+Computes the marginals and messages for cascade of length T on a graph g with initial condition p0
+"""
+function dmp_si(g::Graph, p0::Array{Float64, 1}, T::Int64)
+    marginals = zeros(Float64, T, g.n)
+    return marginals
+end
