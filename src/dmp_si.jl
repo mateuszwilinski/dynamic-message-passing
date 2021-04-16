11b374947d7a713dae9b7eb41089a49f94cc1e0a
mateuszwilinski <mateuszwilinski@o2.pl>
2021-04-16 11:53:46 -0600

    test

--- src/dmp_si.jl
+++ src/dmp_si.jl
@@ -6,5 +6,6 @@ Computes the marginals and messages for cascade of length T on a graph g with in
 """
 function dmp_si(g::Graph, p0::Array{Float64, 1}, T::Int64)
     marginals = zeros(Float64, T, g.n)
+    
     return marginals
 end
