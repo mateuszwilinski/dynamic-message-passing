e8bdc268adfdfe496ed1a254ef6102b30f3182d7
mateuszwilinski <mateuszwilinski@o2.pl>
2021-04-16 23:12:31 -0600

    added si cascade function and fixed the si dmp equations

--- src/dmp_si.jl
+++ src/dmp_si.jl
@@ -17,7 +17,10 @@ function dmp_si(g::Graph, p0::Array{Float64, 1}, T::Int64)
     end
 
     # initial conditions
-    marginals[1, :] = 1.0 - p0
+    marginals[1, :] = 1.0 .- p0
+    for (k, v) in msg_phi
+        v[1] = p0[k[1]]
+    end
 
     # DMP
     for t in 2:T
@@ -35,10 +38,37 @@ function dmp_si(g::Graph, p0::Array{Float64, 1}, T::Int64)
         # theta phi
         for (e, w) in msg_phi
             w[t] = (1.0 - g.edgelist[sort(e)]) * w[t-1]
-            w[t] += marginals[t-1, i] / msg_theta[reverse(e)][t-1]
-            w[t] -= marginals[t, i] / msg_theta[reverse(e)][t]
+            w[t] += marginals[t-1, e[1]] / msg_theta[reverse(e)][t-1]
+            w[t] -= marginals[t, e[1]] / msg_theta[reverse(e)][t]
         end
     end
 
     return marginals, msg_phi, msg_theta
+end
+
+"""
+    cascade(g, p0, T)
+
+Generate a cascade of length T on a graph g with initial condition p0
+"""
+function cascade_si(g::Graph, p0::Array{Float64, 1}, T::Int64)
+    active = zeros(UInt8, T, g.n)
+    active[1, :] = (rand(g.n) .< p0)
+    active[2:end, :] = reshape(repeat(active[1, :] * UInt8(1), inner=T-1), (T-1, g.n))
+    # active[2:end, :] = reshape(repeat(active[1, :] * UInt8(2), inner=T-1), (T-1, g.n))
+    for t in 2:T
+        actives = findall(x -> x == 1, active[t-1, :])  # active nodes
+        for i in actives
+            for j in g.neighbors[i]
+                if active[t, j] .== 0
+                    if rand() < g.edgelist[sort([i, j])]
+                        active[t:end, j] .= 1
+                        # active[t, j] = 1
+                        # active[(t+1):end, j] = repeat(UInt8[2], inner=T-t)
+                    end
+                end
+            end
+        end
+    end
+    return active
 end
