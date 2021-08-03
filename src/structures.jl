4082c57408eebea4c3da6f9a0e8832313a8fb7e4
mateuszwilinski <mateuszwilinski@o2.pl>
2021-08-02 21:51:34 -0600

    added directed dmp equations

--- src/structures.jl
+++ src/structures.jl
@@ -5,3 +5,11 @@ struct Graph
     edgelist::Dict{Array{Int64, 1}, Float64}
     neighbors::Array{Array{Int32, 1}, 1}
 end
+
+struct DirGraph
+    n::Int64
+    m::Int64
+    edgelist::Dict{Array{Int64, 1}, Real}
+    out_neighbors::Array{Array{Int64, 1}, 1}
+    in_neighbors::Array{Array{Int64, 1}, 1}
+end
